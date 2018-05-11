f1dd99fc510ccc8a93dba4b1f14175bd829d4da5
comfykernel <comfykernel@gmail.com>
2018-05-10 23:19:03 -0700

    Added side menu

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+var menuOpen = false;
 
+var node_bodymenu   = document.getElementById("bodymenu"  );
+var node_headbutton = document.getElementById("headbutton");
+var node_headlogo   = document.getElementById("headlogo"  );
+
+function toggleMenu() {
+  if(!menuOpen) {
+    node_bodymenu.classList.remove  ("bodymenu-closed"  );
+    node_headbutton.classList.remove("headbutton-closed");
+    node_headlogo.classList.remove  ("headlogo-closed"  );
+    menuOpen = true;
+  } else {
+    node_bodymenu.classList.add  ("bodymenu-closed"  );
+    node_headbutton.classList.add("headbutton-closed");
+    node_headlogo.classList.add  ("headlogo-closed"  );
+    menuOpen = false;
+  }
+}
